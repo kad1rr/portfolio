8e806f88580d6a4ca6a46b046e3f141a26a34b88
Kadir <k4dircelikk@gmail.com>
2023-10-27 21:03:16 +0300

    Modifies layouts/Head.jsx

--- src/layouts/Head.jsx
+++ src/layouts/Head.jsx
@@ -2,7 +2,7 @@ const Head = () => {
   return (
     <div className="w-full h-auto py-12 bg-secondryBackground flex flex-col items-center justify-around text-center gap-y-6">
       <a
-        href="https://github.com/"
+        href="https://github.com/kad1rr/"
         className="text-[4rem] md:text-[9rem] hover:text-blue-500 transition-colors font-bold text-blue-600"
       >
         KADIR
